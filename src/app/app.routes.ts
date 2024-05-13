ee73096bb95d8b409e03b2786e8092c94f549528
Salma Moubarki <salmamoubarki@gmail.com>
2024-05-13 13:12:24 +0200

    Add commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { MenuComponent } from './menu/menu.component';
+import { SignupComponent } from './signup/signup.component';
+import { UserprofileComponent } from './userprofile/userprofile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: UserprofileComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'userprofile', component: UserprofileComponent },
+
+];
